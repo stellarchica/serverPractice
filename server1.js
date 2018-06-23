7f74b5c76ea417a3ff153347611629a0d048e5c6
sarah martinez <sarahmartinez@sarahs-MacBook.local>
2018-06-23 10:45:54 -0500

    file

---
+++ server1.js
@@ -0,0 +1,25 @@
+// Require/import the HTTP module
+var http = require("http");
+
+// Define a port to listen for incoming requests
+var PORT1 = 7000;
+
+var PORT2 = 7500;
+
+// Create a generic function to handle requests and responses
+function handleRequest(request, response) {
+
+  // Send the below string to the client when the user visits the PORT URL
+  response.end("You have great hair! Read more: " + request.url);
+}
+
+// Use the Node HTTP package to create our server.
+// Pass the handleRequest function to empower it with functionality.
+var server = http.createServer(handleRequest);
+
+// Start our server so that it can begin listening to client requests.
+server.listen(PORT1, PORT2, function() {
+
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
